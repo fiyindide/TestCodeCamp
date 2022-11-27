39bfde09408a44b131c719b5bb1d29c0aba44366
fiyindide <deefiyinfoluwa@gmail.com>
2022-11-27 17:27:50 +0100

    Task 14

---
+++ firstJavaProject/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("A brief description about myself");
+        System.out.println("I am an excellent and certified Software Tester " +
+                "with" +
+                " background in engineering");
+        System.out.println("IT and hands-on experience in Software Testing.");
+        System.out.println("Responsible for quality assurance of software" +
+                " development");
+        System.out.println("and deployment by ensuring products are fit for " +
+                "purpose and fit for use");
+        System.out.println("I love Software testing!");
+    }
+}
